f3d87f5aa184593cdaf7d5688082d51b9bccd8d7
AaBelov <vip.a.belov@mail.ru>
2015-10-26 00:36:21 +0300

    changed readme and small bugfix

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
+#This is the explaination of what the analysis script does
 
+First of all the script merges test and training sets into one dataset.
+Theese parts can be found in X_test.txt and X_train.txt files. 
+Then test and training activity labels and subjects are merged togehter in the same way.
+
+The next step is applying column names so that we can select required ones later.
+Several labels are used more than one time but column names should be unique.
+make.unique function will do the trick.
+Names for activity and subjects dataframes are also added.
+
+Now when columns have unique names we can use dplyr::select function and finally merge all parts in one dataset.
+
+Having the complete dataset created we can replace activity codes with activity labels.
+They are stored in activity_labels.txt file.
+
+After that we should slightly correct column names.
+Lets assume that *thisFormatAsTheMostAppropriate*.
+Column names can be changed with sub and gsub functions.
+
+Then the resulting values and dataset are computed with summarize_each function and written in the result.txt file.
--- run_analysis.R
+++ run_analysis.R
@@ -14,17 +14,31 @@ combined_subjects <- rbind(test_subjects, train_subjects)
 rm(test_subjects, train_subjects)
 
 column_labels <- read.table("UCI HAR Dataset/features.txt", header = FALSE)
-new_names <- make.names(as.character(column_labels$V2), unique = TRUE, allow_ = TRUE)
+new_names <- make.unique(as.character(column_labels$V2), sep = ".")
 colnames(combined_data) <- new_names
 colnames(combined_subjects) <- "subject"
 colnames(combined_activity) <- "activity"
 rm(column_labels, new_names)
 
-sel_mean <- dplyr::select(combined_data, contains("mean"))
-sel_std <- dplyr::select(combined_data, contains("std"))
+sel_mean <- dplyr::select(combined_data, contains("mean()"))
+sel_std <- dplyr::select(combined_data, contains("std()"))
 selected_data <- cbind(sel_mean, sel_std)
 data <- cbind(combined_subjects, combined_activity, selected_data)
 rm(selected_data, sel_std, sel_mean, combined_subjects, combined_activity, combined_data)
 
 activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt", header = FALSE, stringsAsFactors = FALSE)
 data$activity <- plyr::mapvalues(data$activity, activity_labels$V1, activity_labels$V2)
+
+n <- names(data)
+n <- sub("tBody", "timeBody", n, fixed = TRUE)
+n <- sub("fBody", "frequencyBody", n, fixed = TRUE)
+n <- sub("tGravity", "timeGravity", n, fixed = TRUE)
+n <- gsub("mean", "Mean", n, fixed = TRUE)
+n <- gsub("std", "Std", n, fixed = TRUE)
+n <- gsub("()", "", n, fixed = TRUE)
+n <- gsub("-", "", n, fixed = TRUE)
+names(data) <- n
+
+library(dplyr)
+result <- summarize_each(group_by(data, activity, subject), funs(mean))
+write.table(result, "result.txt", row.names = FALSE)
